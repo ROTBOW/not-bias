8dcf102be35f4df8674f116e3456d73cf6e149bc
Josiah_L <sjosiahleon@gmail.com>
2023-03-17 16:31:06 -0700

    -- init -- Started project

---
+++ core.py
@@ -0,0 +1,7 @@
+
+class Core:
+    
+    def __init__(self, refresh):
+        self.refresh = refresh
+        
+    
---
+++ data.py
@@ -0,0 +1,8 @@
+items = [
+    '''
+    Grief is really just love.
+    It's all the love you want to give but cannot.
+    
+    Grief is just love with no place to go.
+    ''',
+]

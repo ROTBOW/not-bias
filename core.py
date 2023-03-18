5b44fbe6c2812ee8021b9b81fb74883ee72fd194
Josiah_L <sjosiahleon@gmail.com>
2023-03-17 23:15:50 -0700

    added typing added more poems added typing sound effect added volume control for rain

--- core.py
+++
@@ -1,7 +0,0 @@
-
-class Core:
-    
-    def __init__(self, refresh):
-        self.refresh = refresh
-        
-    
--- data.py
+++ data.py
@@ -1,8 +1,66 @@
-items = [
-    '''
-    Grief is really just love.
+quotes = [
+    '''Grief is really just love.
     It's all the love you want to give but cannot.
     
-    Grief is just love with no place to go.
-    ''',
+    Grief is just love with no place to go.''',
+    
+    '''sometimes the hardest part is knowing that
+    we will never have enough time with each other
+    
+    it will always be too soon to lose you.''',
+    
+    '''they say
+    it won't hurt forever
+    but i will never
+    exprerience forever.
+    
+    so tell me,
+    how much longer
+    do i have live
+    with this pain?''',
+    
+    '''i can never be
+    just friends
+    with you
+    maybe because your eyes
+    remind me of the nights we spent together
+    i cant look in the eyes i once saw a future together in
+    and pretend to see nothing''',
+    
+    '''i thought a love like ours would last a lifetime.
+    it was a happily ever after kind of love.
+    the kind where our laugh started to sound the
+    same and we knew each other by heart,
+    but somewhere along the way that wasn't enough
+    to keep love on our side.
+    
+    where did we go wrong
+    when everything felt so right?
+    
+    yesterday feels so far away now
+    and i don't know if i'll ever be able
+    to wake up tomorrow and not miss you.''',
+    
+    '''PICTURES ARE THE BONES OF GOOD MOMENTS
+    
+    what is a memory
+    if not a ghost?
+    
+    i see us there smilling -
+    and all it does it haunt me.''', 
+    
+    '''meeting you was fate.
+    becoming your friend was a choice
+    
+    falling in love with you was
+    beyond my control.''',
+    
+    '''the truth is,
+    i don't talk about it anymore.
+    but that doesn't mean it stopped hurting.''',
+    
+    '''i hold on to the past
+    so i can keep running into you
+    
+    i hope you never forget everything i'll always remember.'''
 ]

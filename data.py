66673b59988d73be5de8e30a8a284f41f7893d8a
Josiah_L <sjosiahleon@gmail.com>
2023-03-17 23:50:14 -0700

    name change tiny moble responcething added rest of quotes

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ quotes = [
     '''they say
     it won't hurt forever
     but i will never
-    exprerience forever.
+    experience forever.
     
     so tell me,
     how much longer
@@ -46,7 +46,7 @@ quotes = [
     what is a memory
     if not a ghost?
     
-    i see us there smilling -
+    i see us there smiling -
     and all it does it haunt me.''', 
     
     '''meeting you was fate.
@@ -62,5 +62,147 @@ quotes = [
     '''i hold on to the past
     so i can keep running into you
     
-    i hope you never forget everything i'll always remember.'''
+    i hope you never forget everything i'll always remember.''',
+    
+    '''i gave you my heart
+    because i knew
+    that it didn't belong anywhere else.''',
+    
+    '''does anybody know where the switch is?
+    the switch to turn these feelings off
+    because i'm tired of feeling them so deeply
+    
+    i'm tired of wearing my heart out on my sleeve
+    with a sign that reads "warning: i may love hard"
+    and i'm tired of those that come into my life experience my love
+    and leave
+    because, for them, it's too much
+    so they make it difficult for me to even want to love hard
+    
+    i'm tired of my heart catching the holy ghost
+    for souls that visit this temple without intending to stay
+    i'm tired of my heart acting like a shelter
+    and eventually go astray
+    
+    i don't want to feel no more
+    at least, not so much
+    for once i just want to feel
+    when i feel the feeling is mutual
+    for once i just want to feel
+    
+    when i feel the feeling is mutual''',
+    
+    '''Just when I thought
+    I had finally forgotten you,
+    sleep took over,
+    and we were dancing again.''',
+    
+    '''i can't believe
+    how quickly we went
+    from everything
+    to nothing''',
+    
+    '''it was only after you left
+    that i realized
+    how empty it feels
+    without you''',
+    
+    '''I don't know what they are called,
+    the spaces between seconds-
+    but I think of you always in those intervals.''',
+    
+    '''for you i was a chapter,
+    for me you were the book.''',
+    
+    '''And why shouldn't I love you like you are everything I have ever wanted from this life.''',
+    
+    '''it's kinda weird that
+    we don't exist
+    in each other's life
+    anymore.
+    
+    it's like nothing
+    has ever happened
+    between us.
+    
+    but how can it be nothing
+    when it was everything
+    at one point in time?''',
+    
+    
+    '''you came into my world
+    made me develop feelings for you
+    and then you left
+    you left left as though my feelings would leave as quickly as you
+    
+    and i sort of wish they did.''',
+    
+    '''I have late night conversations
+    with the moon,
+    he tells me about the sun
+    and I tell him about you.''',
+    
+    '''i pray that someday
+    we will meet each other again
+    for the first time.
+    
+    i hope nex time we won't be an almost.''',
+    
+    '''sometimes i wish
+    i never met you
+    so i wouldn't
+    know the feeling
+    of knowing you
+    but not being able to
+    have you around''',
+    
+    '''I just wanted you to know
+    That I'll never care
+    how dar you pushed me away
+    Because when I told you
+    That I would stay
+    I meant it
+    you're a little lost
+    And a little damaged
+    But you're not hopeless
+    I know who you are
+    I love who you are
+    And that's why I'll stay
+    So you learn to love
+    Yourself too''',
+    
+    '''i don't know to forget you
+    but I don't know how to
+    think about you
+    without it hurting...''',
+    
+    '''when i thought about telling you
+    how much i missed you,
+    i realized that wouldn't be fair
+    to either one of us.''',
+    
+    '''could you come back please
+    and just love me a little longer
+    i don't need much
+    just give my hand a squeeze
+    kiss my forehead
+    could you please come back
+    just for a second
+    just love me for this instance
+    i just want to remember this a little better
+    i just want to make sure i don't forget.''',
+    
+    '''maybe in another lifetime
+    i will find you again
+    and it will be different.''',
+    
+    '''Don't overthink it and
+    just call me when you miss me.''',
+    
+    '''i didn't sign up for heartbreak
+    i signed up for love
+    but somehow i received both
+    you loved my heart
+    
+    and then you broke it'''
 ]
